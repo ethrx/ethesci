203e6d075c228f6e0ec2878cefe85df07f2e4a04
Ethan <70865398+ethrx@users.noreply.github.com>
2021-11-10 12:54:24 +0000

    Update index.js

--- index.js
+++ index.js
@@ -2,7 +2,6 @@ const Web3 = require("web3")
 class erc721 {
   constructor(network, api, address) {
     this.mainnet = network
-    //this.erc721 = api
     this.web3 = new Web3(new Web3.providers.HttpProvider(this.mainnet));
     this.Contract = new this.web3.eth.Contract(api, address);
   }
@@ -36,5 +35,25 @@ class erc721 {
     return tokens
   }
 }
+class contract {
+  constructor(network, api, address) {
+    this.mainnet = network
+    this.web3 = new Web3(new Web3.providers.HttpProvider(this.mainnet));
+    this.Contract = new this.web3.eth.Contract(api, address);
+  }
+  contractCall(func, args) {
+    if (Array.isArray(args)) {
+      let funcStr = "this.Contract.methods[func](" + JSON.stringify(args).replace("[", "").replace("]", "") + ")"
+      return eval(funcStr)
+    }
+    // in case someone decides to pass a single arg
+    else {
+      return this.Contract.methods[func](args)
+    }
+  }
+}
 
-module.exports.erc721 = erc721
+module.exports = {
+  erc721,
+  default: contract
+}
